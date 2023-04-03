190dc7964288b338a58e8826f7fd4dd94916e1b6
Admin <Admin@DESKTOP-5AB338K>
2023-04-03 12:45:40 +0530

    1st commit

---
+++ src/test/java/mvn_sales/Sample.java
@@ -0,0 +1,13 @@
+package mvn_sales;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test(groups="smoke")
+	public void delete()
+	{
+		System.out.println("deleted successfully");
+	}
+
+}
---
+++ src/test/java/mvn_sales/SampleTest.java
@@ -0,0 +1,18 @@
+package mvn_sales;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test(groups="smoke")
+	public void create()
+	{
+		System.out.println("created successfully");
+	}
+	@Test(groups="regression")
+	public void modify()
+	{
+		System.out.println("modified regression");
+	}
+
+}
